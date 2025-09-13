ac4242b73a3954f1206ccfedd4cff95f09ddf980
syedtalhahamid <syedtalhacodenera@gmail.com>
2025-09-13 12:41:17 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('localhost', 12345))
+server.bind(('0.0.0.0', 12345))
 server.listen()
 
 print("Server started... Waiting for connection...")
