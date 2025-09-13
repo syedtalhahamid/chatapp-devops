3384df4a0eed41d85696740a9fdda232179365e0
syedtalhahamid <syedtalhacodenera@gmail.com>
2025-09-13 12:40:56 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('localhost', 12345))
+client.connect(('54.90.214.143', 12345))
 
 client.sendall(b"Hello server!")
 data = client.recv(1024)
